281f17caad4d372aa8ecf9f26f7a4b41a078405d
Sumedh Salvi <sumedhsalvi25@gmail.com>
2023-04-09 15:48:30 -0400

    doc coverage for api and user json change

--- movies-search-engine-api/controller.py
+++ movies-search-engine-api/controller.py
@@ -31,6 +31,11 @@ JWT_EXPIRATION_TIME = timedelta(hours=1)
 
 @app.route('/login', methods=['POST'])
 def login():
+    """
+    Authenticates a user and generates a JSON Web Token (JWT) if the authentication is successful.
+    Returns:
+        A JSON response containing a JWT if the authentication is successful, or an error message if the authentication fails.
+    """    
     auth = request.authorization
 
     if not auth or not auth.username or not auth.password:
@@ -56,6 +61,15 @@ def login():
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
+    """
+    Returns a list of movies based on query parameters passed in the request URL.
+    Query Parameters:
+        genres (str): A comma-separated list of movie genres to filter by.
+        averageRating (float): The minimum average rating a movie must have to be included in the results.
+        yearRange (str): A range of years to filter by, in the format "start_year,end_year".
+    Returns:
+        A JSON response containing a list of movies that satisfy the query parameters.
+    """    
     genres = request.args.get('genres')
     min_rating = request.args.get('averageRating')
     year_range = request.args.get('yearRange')
@@ -63,34 +77,22 @@ def get_movies():
     # filter the movies based on the query parameters
     filtered_movies = []
     for name, movie in new_movie_data.items():
-        # if genres and genres not in movie['genre']:
-        #     continue
-        # if min_rating and movie['average_rating'] < float(min_rating):
-        #     continue
         if year_range:
             start_year, end_year = map(int, year_range.split(','))
-        #     if movie_year) < start_year or movie_year) > end_year:
-        #         continue
-
-        if movie['year'] and movie['year'].isnumeric():
-            movie_year = int(movie['year'])
-        else:
-            movie_year = 0
-
 
         # Check if the movie satisfies all three conditions
         if genres and min_rating and year_range:
-            if genres in movie['genre'] and movie['average_rating'] >= float(min_rating) and start_year <= movie_year <= end_year:
+            if genres in movie['genre'] and movie['average_rating'] >= float(min_rating) and start_year <= movie['year'] <= end_year:
                 filtered_movies.append({'name': name, **movie})
         # Otherwise, if there are only two conditions, check if they are satisfied
         elif genres and min_rating:
             if genres in movie['genre'] and movie['average_rating'] >= float(min_rating):
                 filtered_movies.append({'name': name, **movie})
         elif genres and year_range:
-            if genres in movie['genre'] and start_year <= movie_year <= end_year:
+            if genres in movie['genre'] and start_year <= movie['year'] <= end_year:
                 filtered_movies.append({'name': name, **movie})
         elif min_rating and year_range:
-            if movie['average_rating'] >= float(min_rating) and start_year <= movie_year <= end_year:
+            if movie['average_rating'] >= float(min_rating) and start_year <= movie['year'] <= end_year:
                 filtered_movies.append({'name': name, **movie})
         # If only one condition is provided, simply add the movie to the list
         else:
